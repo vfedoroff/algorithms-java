42aed41f7b2e1f1fe68350554829e83b0315206b
Vadym Fedorov <vfedorov@beemka.org>
2021-08-14 22:23:10 -0500

    Merge Two Sorted Arrays

---
+++ src/test/java/educative/CheckMergeArrayTest.java
@@ -0,0 +1,43 @@
+package educative;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class CheckMergeArrayTest {
+  public static int[] mergeArrays(int[] arr1, int[] arr2) {
+    int l1 = arr1.length;
+    int l2 = arr2.length;
+    int size = l1 + l2;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int[] out = new int[size];
+    while (i < l1 && j < l2) {
+      int a = arr1[i];
+      int b = arr2[j];
+      if (a < b) {
+        out[k++] = a;
+        ++i;
+      } else {
+        out[k++] = b;
+        ++j;
+      }
+    }
+    while (i < l1)
+      out[k++] = arr1[i++];
+    // Store remaining elements of second array
+    while (j < l2)
+      out[k++] = arr2[j++];
+    return out;
+  }
+
+  @Test
+  void testMergeArrays() {
+    int[] arr1 = new int[] {1, 3, 4, 5};
+    int[] arr2 = new int[] {2, 6, 7, 8};
+    int[] actual = mergeArrays(arr1, arr2);
+    assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 7, 8}, actual);
+  }
+}
